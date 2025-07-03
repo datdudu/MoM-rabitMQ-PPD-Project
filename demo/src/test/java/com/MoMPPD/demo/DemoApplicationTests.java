3148beaf0f8d23a53cab7fd30de084e4aacd4fb4
datdudu <c.eduardocarvalhoc@gmail.com>
2025-07-03 12:25:38 -0300

    first commit

---
+++ demo/src/test/java/com/MoMPPD/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MoMPPD.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
